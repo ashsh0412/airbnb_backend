8b4224e723a42bae9b46fd96ea1fc04abaf3f170
ashsh0412 <ashsh0412@gmail.com>
2024-07-28 21:59:40 +0900

    Photo serializer

--- rooms/views.py
+++ rooms/views.py
@@ -15,6 +15,7 @@ from rest_framework.status import (
 from categories.models import Category
 from django.db import transaction
 from reviews.serializers import ReviewSerializer
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -234,5 +235,22 @@ class RoomAmenities(APIView):
 
 class RoomPhotos(APIView):
 
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error_messages)
