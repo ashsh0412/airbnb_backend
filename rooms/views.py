3a575c09066da6d835e151d8668de7ed7b2494cd
ashsh0412 <ashsh0412@gmail.com>
2024-08-01 23:27:26 +0900

    bookings

--- rooms/views.py
+++ rooms/views.py
@@ -17,6 +17,9 @@ from django.db import transaction
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from bookings.models import Booking
+from bookings.serializers import PublicBookingSerializer
+from django.utils import timezone
 
 
 class Amenities(APIView):
@@ -261,5 +264,38 @@ class RoomPhotos(APIView):
             photo = serializer.save(room=room)
             serializer = PhotoSerializer(photo)
             return Response(serializer.data)
+        else:
+            return Response(serializer.error_messages)
+
+
+class RoomBookings(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializers = PublicBookingSerializer(
+            bookings,
+            many=True,
+        )
+        return Response(serializers.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = PublicBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            pass
         else:
             return Response(serializer.error_messages)
